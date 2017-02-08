e80a8a9a1a19b63561d6e786b60e2e0c566f5f61
Quynh Nguyen <quynhnh@gmail.com>
2017-02-08 18:03:02 +0000

    Initial commit

---
+++ CleanStockList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using RestSharp;
+
+namespace CleanStockList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world from CleanStockList");
+
+            Console.WriteLine("Start cleaning NYSE");
+            ProcessFile("StocksNYSE.txt");
+
+            Console.WriteLine("Start cleaning NASDAQ");
+            ProcessFile("StocksNASDAQ.txt");
+
+            Console.WriteLine("All done");
+            Console.ReadLine();
+        }
+
+        static void ProcessFile(string filePath)
+        {
+            //Read the list of code into memory
+            var symbols = File.ReadLines(filePath).Select(line =>
+            {
+                var elements = line.Split('\t');
+                return elements[1];
+            }).Distinct().ToList().Take(10);
+
+            //Prepare Restsharp
+            var client = new RestClient("http://dev.markitondemand.com/");
+            //var request = new RestRequest("/MODApis/Api/v2/Quote?symbol={code}", Method.GET);
+
+            var stocks = new List<StockQuote>();
+            foreach (var symbol in symbols)
+            {
+                //request.AddUrlSegment("code", symbol);
+                var request = new RestRequest($"/MODApis/Api/v2/Quote?symbol={symbol}", Method.GET);
+                var response = client.Execute<StockQuote>(request);
+                var stock = response.Data;
+
+                if (!string.IsNullOrEmpty(stock.Name))
+                {
+                    stocks.Add(stock);
+                }
+            }
+
+            //var stocks = symbols.Select(symbol =>
+            //{
+            //    request.AddUrlSegment("code", symbol);
+            //    var response = client.Execute<StockQuote>(request);
+            //    return response.Data;
+            //}).Where(foo => !string.IsNullOrEmpty(foo.Name)).ToList();
+
+            //Write into a target file
+            var targetFileName = Path.GetFileNameWithoutExtension(filePath) + "_Valid.txt";
+            File.WriteAllLines(targetFileName, stocks.Select(stock =>
+            {
+                return String.Format($"{stock.Symbol}\t{stock.Name}");
+            }));
+        }
+    }
+}
---
+++ CleanStockList/StockQuote.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanStockList
+{
+    class StockQuote
+    {
+
+        public string Status
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Symbol
+        {
+            get;
+            set;
+        }
+        public double LastPrice
+        {
+            get;
+            set;
+        }
+
+        public double Change
+        {
+            get;
+            set;
+        }
+
+        public double ChangePercent
+        {
+            get;
+            set;
+        }
+
+        public string Timestamp
+        {
+            get;
+            set;
+        }
+
+        public double MSDate
+        {
+            get;
+            set;
+        }
+
+        public double MarketCap
+        {
+            get;
+            set;
+        }
+
+        public Int32 Volume
+        {
+            get;
+            set;
+        }
+
+        public double ChangeYTD
+        {
+            get;
+            set;
+        }
+
+        public double ChangePercentYTD
+        {
+            get;
+            set;
+        }
+
+        public double High
+        {
+            get;
+            set;
+        }
+
+        public double Low
+        {
+            get;
+            set;
+        }
+
+        public double Open
+        {
+            get;
+            set;
+        }
+    }
+}
